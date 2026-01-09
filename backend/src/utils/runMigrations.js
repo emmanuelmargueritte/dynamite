c0291a43e546652d7dbb0f86cefef6f0d2db541b
emmanuelmargueritte <emmanuel.margueritte@gmail.com>
2026-01-09 21:03:05 +1100

    Fix migrations.sql relative path

--- backend/src/utils/runMigrations.js
+++ backend/src/utils/runMigrations.js
@@ -4,7 +4,11 @@ const path = require('path');
 const { pool } = require('./db');
 
 (async () => {
-  const sql = fs.readFileSync(path.join(__dirname, '../../../sql/migrations.sql'), 'utf8');
+  const sql = fs.readFileSync(
+    path.join(__dirname, '../../sql/migrations.sql'),
+    'utf8'
+  );
+
   await pool.query(sql);
   console.log('✅ Migrations exécutées');
   process.exit(0);
